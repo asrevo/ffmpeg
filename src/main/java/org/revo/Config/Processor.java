adb4b90f6723464f70f36ece0d37edd9c3c45ca5
ashraf <ashraf1abdelrasool@gmail.com>
2018-11-29 17:41:09 +0200

    push all :fire:

--- src/main/java/org/revo/Config/Processor.java
+++ src/main/java/org/revo/Config/Processor.java
@@ -9,27 +9,27 @@ public interface Processor {
     String ffmpeg_queue = "ffmpeg_queue";
 
     @Input("ffmpeg_queue")
-    MessageChannel ffmpeg_queue();
+    SubscribableChannel ffmpeg_queue();
 
     String ffmpeg_converter_pop = "ffmpeg_converter_pop";
 
     @Input("ffmpeg_converter_pop")
-    MessageChannel ffmpeg_converter_pop();
+    SubscribableChannel ffmpeg_converter_pop();
 
     String ffmpeg_converter_push = "ffmpeg_converter_push";
 
     @Output("ffmpeg_converter_push")
-    SubscribableChannel ffmpeg_converter_push();
+    MessageChannel ffmpeg_converter_push();
 
     String bento4_hls = "bento4_hls";
 
     @Output("bento4_hls")
-    SubscribableChannel bento4_hls();
+    MessageChannel bento4_hls();
 
     String tube_info = "tube_info";
 
     @Output("tube_info")
-    SubscribableChannel tube_info();
+    MessageChannel tube_info();
 
     String feedBack_push = "feedBack_push";
 
